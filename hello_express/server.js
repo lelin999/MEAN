5b204d80846b2941ec42ba72d6c05c1cacd0e18a
Le Lin <lelin999@gmail.com>
2017-04-13 11:18:56 -0700

    intro done

---
+++ hello_express/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+// This sets the location where express will look for the ejs views
+app.set('', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.send("Hello world");
+    console.log(app.settings)
+});
+
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
+
+app.listen(8000, function() {
+	console.log("this shit works")
+});
